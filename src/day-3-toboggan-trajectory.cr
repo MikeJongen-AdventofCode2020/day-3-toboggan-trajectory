6d5b4807000c5a5490eeab07b80c3caa0296c101
MikeJongenVention <mike.jongen@vention.nl>
2020-12-03 10:23:13 +0100

    Added argument handling and file read.

--- src/day-3-toboggan-trajectory.cr
+++ src/day-3-toboggan-trajectory.cr
@@ -1,6 +1,28 @@
-# TODO: Write documentation for `Day::3::Toboggan::Trajectory`
-module Day::3::Toboggan::Trajectory
-  VERSION = "0.1.0"
+require "option_parser"
+require "benchmark"
 
-  # TODO: Put your code here
+file_name = ""
+benchmark = false
+
+OptionParser.parse do |parser|
+  parser.banner = "Welcome to Report Repair"
+
+  parser.on "-f FILE", "--file=FILE", "Input file" do |file|
+    file_name = file
+  end
+  parser.on "-b", "--benchmark", "Measure benchmarks" do
+    benchmark = true
+  end
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+end
+
+unless file_name.empty?
+  map = File.read_lines(file_name)
+  
+  result = 0
+
+  puts result
 end
